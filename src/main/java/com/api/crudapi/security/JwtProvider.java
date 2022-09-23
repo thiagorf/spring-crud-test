453acf569bda87dabd21188a9747201c687e4943
thiagorf <trfarciroli@gmail.com>
2022-09-23 12:03:03 -0300

    change Bearer header aprach to cookie based

--- src/main/java/com/api/crudapi/security/JwtProvider.java
+++ src/main/java/com/api/crudapi/security/JwtProvider.java
@@ -3,6 +3,7 @@ package com.api.crudapi.security;
 import java.util.Date;
 import java.util.function.Function;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.api.crudapi.user.UserModel;
@@ -14,17 +15,18 @@ import io.jsonwebtoken.SignatureAlgorithm;
 @Service
 public class JwtProvider {
 
-	private String SECRET_UNSAFE_KEY = "c2VjcmV0";
+	@Value("${api.secret}")
+	private String SECRET;
 
 	public String sign(UserModel user) {
 		var jwt = Jwts.builder().setSubject(user.getEmail()).setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-				.signWith(SignatureAlgorithm.HS256, SECRET_UNSAFE_KEY).compact();
+				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60))
+				.signWith(SignatureAlgorithm.HS256, SECRET).compact();
 		return jwt;
 	}
 
 	public Claims verify(String token) {
-		var jwt = Jwts.parser().setSigningKey(SECRET_UNSAFE_KEY).parseClaimsJws(token).getBody();
+		var jwt = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
 		return jwt;
 	}
 
